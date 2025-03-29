055dfdaeff78bdeb35ac052f06e9fb6391e06c3c
etharalrehaili <e.abdullah.re@gmail.com>
2025-03-29 10:25:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/introtoxmllayouts/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.introtoxmllayouts
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout)
+
+        enableEdgeToEdge()
+
+    }
+}
